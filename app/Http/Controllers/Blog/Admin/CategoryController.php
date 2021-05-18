e2d2d831b0b90ca322ac8c5d7fdeee4a9b47d70b
sytnic <sytnic@list.ru>
2021-05-18 15:29:21 +0300

    13, Admin controllers created

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Blog\Admin\BaseController;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // проверка
+        dd(__METHOD__);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // show не будет работать, 
+        // если он не задан в routes/wep.php
+        // проверка
+        dd(__METHOD__);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // проверка
+        dd(__METHOD__);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/Controllers/Blog/BaseController.php
+++ app/Http/Controllers/Blog/BaseController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Blog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// Это базовый контроллер для блога
+// и его подчиненных (внутренних) контроллеров.
+// Он наследуется от общего Контроллера.
+// Он не используется напрямую, от него только идёт наследование "вниз" (abstract)
+// для подчиненных контроллеров.
 abstract class BaseController extends Controller
 {
     //
